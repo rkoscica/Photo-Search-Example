c9cd06211a3bc074036117ee84bc3ef2327af3b6
Rosalyn Koscica <rkoscica@rkoscica-2016.local>
2015-12-08 09:49:18 -0500

    thinkful 4.4.1 error on "manager.GET"

--- Photo Search Example/ViewController.swift
+++ Photo Search Example/ViewController.swift
@@ -5,21 +5,32 @@
 //  Created by Rosalyn Koscica on 12/8/15.
 //  Copyright © 2015 Rosalyn Koscica. All rights reserved.
 //
+//
+// Client ID: d0a70d0738474a27b6259a6ff0c82475
+
 
 import UIKit
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let manager = AFHTTPRequestOperation()
+        manager.GET( "https://api.instagram.com/v1/tags/cats/media/recent?client_id=d0a70d0738474a27b6259a6ff0c82475",
+            parameters: nil,
+            success: { (operation: AFHTTPRequestOperation?,responseObject: AnyObject!) in
+                print("JSON: " + responseObject.description)
+            },
+            failure: { (operation: AFHTTPRequestOperation?,error: NSError!) in
+                print("Error: " + error.localizedDescription)
+        })
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
---
+++ bridge.h
@@ -0,0 +1,19 @@
+//
+//  bridge.h
+//  Photo Search Example
+//
+//  Created by Rosalyn Koscica on 12/8/15.
+//  Copyright © 2015 Rosalyn Koscica. All rights reserved.
+//
+
+
+
+#ifndef bridge_h
+#define bridge_h
+
+
+#endif /* bridge_h */
+
+#import "/Users/rkoscica/Desktop/Code12/Photo Search Example/AFNetworking/AFNetworking.h"
+
+#import "AFNetworking/AFNetworking.h"

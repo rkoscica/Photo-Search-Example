e29ba2767507663a99bb78f2f3f049b63948efb1
Rosalyn Koscica <rkoscica@rkoscica-2016.local>
2015-12-14 14:36:53 -0500

    successfully gets data

--- Photo Search Example/ViewController.swift
+++ Photo Search Example/ViewController.swift
@@ -13,17 +13,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let manager = AFHTTPRequestOperationManager()
-        manager.GET( "https://api.instagram.com/v1/tags/cat/media/recent?client_id=d0a70d0738474a27b6259a6ff0c82475",
-            parameters: nil,
-            success: { (operation: AFHTTPRequestOperation?,responseObject: AnyObject!) in
-                print("JSON: " + responseObject.description)
-            },
-            failure: { (operation: AFHTTPRequestOperation?,error: NSError!) in
+
+        let manager = AFHTTPSessionManager()
+        manager.GET("https://api.instagram.com/v1/tags/clararockmore/media/recent?client_id=c4fc61c4704949baab8825cf178e13fe", parameters: nil, success: { (task: NSURLSessionDataTask, responseObject: AnyObject?) -> Void in
+                print("JSON: " + responseObject!.description)
+            
+            }) { (task: NSURLSessionDataTask?, error: NSError) -> Void in
                 print("Error: " + error.localizedDescription)
-        })
+            
+        }
+     
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
